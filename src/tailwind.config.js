e57638c0fe46bb9bd5772b8a52f0be15c79bdc37
Kiprotich marion <marionjemutai71@gmail.com>
2024-11-08 16:21:22 +0300

    Create tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      "twitter-blue": "#IDAIF2",
+    },
+    extend:{},
+  },
+  },
+  plugins: [],
+}
+
